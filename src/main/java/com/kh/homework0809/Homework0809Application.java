7ce54470ed374a3b1ae69149f35984337b1e633d
dudwo2199 <59724504+dudwo2199@users.noreply.github.com>
2025-08-29 16:43:27 +0900

    숙제

---
+++ src/main/java/com/kh/homework0809/Homework0809Application.java
@@ -0,0 +1,13 @@
+package com.kh.homework0809;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework0809Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework0809Application.class, args);
+	}
+
+}
